965485532d879ea7e9a5bed8fe4e944e8997738d
Albano <albigoal@outlook.com>
2024-09-24 22:06:42 +0200

    Add button functionality to ContentView

--- Notes-App/ContentView.swift
+++ Notes-App/ContentView.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var text = "Hello, world!"
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(text)
+            Button(action: {
+                text = "Button pressed!"
+            }) {
+                Text("Press Me")
+            }
+            .padding()
         }
         .padding()
     }
